de6663f95485ddc5034616a0f7ffc99778409c93
mio kataoka <mio.kanna@gmail.com>
2022-05-14 20:38:04 -0700

    commit tree 4b015510cffaebd70c0d8fb01a51b0896ed0f52b

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [searchedPlant, setSearchedPlant] = useState("")
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(items => setPlants(items))
+  }, [])
+
+  function addNewPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+    const displayedPlants = plants.filter((plant) => {
+      return plant.name.toLowerCase().includes(searchedPlant.toLowerCase());
+    });
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} addNewPlant={addNewPlant}/>
+      <Search searchedPlant={searchedPlant} setSearchedPlant={setSearchedPlant} />
+      <PlantList plants={displayedPlants} setPlants={setPlants} />
     </main>
   );
 }
-
 export default PlantPage;
